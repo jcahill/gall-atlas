47716570ed65525f5ae52d22d0e6f9183ac0d082
Jeremy Cahill <j-spam@metamerlabs.io>
2021-08-23 03:13:24 -0400

    Remove fork-me button and fix typo on dark/light toggle.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ export const Footer: React.FC<{
           <a
             className={styles.toggleDarkMode}
             onClick={toggleDarkModeCb}
-            title='Tottle dark mode'
+            title='toggle dark mode'
           >
             {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
           </a>
